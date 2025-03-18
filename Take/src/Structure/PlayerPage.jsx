5a95915708bc6435cf81e9badb4e55cd8f9e8bd1
Sumit Roy <147255172+sum1t7@users.noreply.github.com>
2025-03-18 09:45:44 +0530

    DepUpdate

--- Take/src/Structure/PlayerPage.jsx
+++ Take/src/Structure/PlayerPage.jsx
@@ -1,8 +1,10 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useState } from "react";
 const PlayerPage = ({ type }) => {
   const { id, season, episode } = useParams();
+  const navigate = useNavigate();
   const [server, setServer] = useState(import.meta.env.VITE_SERVER_URL_5);
+  const [count , setCount] = useState(1);
   const choice = [
     import.meta.env.VITE_SERVER_URL,
     import.meta.env.VITE_SERVER_URL_2,
@@ -28,11 +30,12 @@ const PlayerPage = ({ type }) => {
         allowFullScreen
       ></iframe>
       <div className="flex flex-row flex-wrap justify-center items-center">
-        <div className="p-2 cursor-pointer text-amber-600" onClick={() => setServer(choice[4])}> Server 1</div>
-        <div className="p-2 cursor-pointer text-blue-600" onClick={() => setServer(choice[1])}> Server 2</div>
-        <div className="p-2 cursor-pointer text-cyan-600" onClick={() => setServer(choice[2])}> Server 3</div>
-        <div className="p-2 cursor-pointer text-emerald-600" onClick={() => setServer(choice[3])}> Server 4</div>
-        <div className="p-2 cursor-pointer text-fuchsia-600" onClick={() => setServer(choice[0])}> Server 5</div>
+        <div className="p-2 cursor-pointer text-amber-600" onClick={() => {setServer(choice[4]); setCount(count + 1)}}> Server 1</div>
+        <div className="p-2 cursor-pointer text-blue-600" onClick={() => {setServer(choice[1]); setCount(count + 1)}}> Server 2</div>
+        <div className="p-2 cursor-pointer text-cyan-600" onClick={() => {setServer(choice[2]); setCount(count + 1)}}> Server 3</div>
+        <div className="p-2 cursor-pointer text-emerald-600" onClick={() => {setServer(choice[3]); setCount(count + 1)}}> Server 4</div>
+        <div className="p-2 cursor-pointer text-fuchsia-600" onClick={() => {setServer(choice[0]); setCount(count + 1)}}> Server 5</div>
+        <div className="p-2 cursor-pointer text-white" onClick={() => navigate(-count) }> Go Back</div>
       </div>
     </div>
   );
