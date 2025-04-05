05175ea1707b75eb1d4b9cbb4e1774d9c5c32e8d
Sumit Roy <147255172+sum1t7@users.noreply.github.com>
2025-04-05 22:50:39 +0530

    serverUpdate

--- Take/src/Structure/PlayerPage.jsx
+++ Take/src/Structure/PlayerPage.jsx
@@ -16,6 +16,8 @@ const PlayerPage = ({ type }) => {
     import.meta.env.VITE_SERVER_URL_3,
     import.meta.env.VITE_SERVER_URL_4,
     import.meta.env.VITE_SERVER_URL_5,
+    import.meta.env.VITE_SERVER_URL_6,
+
   ];
 
  
@@ -57,11 +59,13 @@ if(!tvData && !type){
         ></iframe>
 
       <div className="flex flex-row flex-wrap justify-center items-center">
-        <div className="p-2 cursor-pointer  text-amber-600" onClick={() => {setServer(choice[4]);  }}> Server 1</div>
-        <div className="p-2 cursor-pointer text-blue-600" onClick={() => {setServer(choice[1]);  }}> Server 2</div>
-        <div className="p-2 cursor-pointer text-cyan-600" onClick={() => {setServer(choice[2]);  }}> Server 3</div>
-        <div className="p-2 cursor-pointer text-emerald-600" onClick={() => {setServer(choice[3]);  }}> Server 4</div>
-        <div className="p-2 cursor-pointer text-fuchsia-600" onClick={() => {setServer(choice[0]);  }}> Server 5</div>
+        <div className="p-2 cursor-pointer text-cyan-500" onClick={() => {setServer(choice[5]);  }}> Server 1</div>
+        <div className="p-2 cursor-pointer  text-amber-600" onClick={() => {setServer(choice[4]);  }}> Server 2</div>
+        <div className="p-2 cursor-pointer text-blue-600" onClick={() => {setServer(choice[1]);  }}> Server 3</div>
+        <div className="p-2 cursor-pointer text-cyan-600" onClick={() => {setServer(choice[2]);  }}> Server 4</div>
+        <div className="p-2 cursor-pointer text-emerald-600" onClick={() => {setServer(choice[3]);  }}> Server 5</div>
+        <div className="p-2 cursor-pointer text-fuchsia-600" onClick={() => {setServer(choice[0]);  }}> Server 6</div>
+
         <div className="p-2 cursor-pointer text-white" onClick={() => navigate(`/${type ? "movie" : "tv"}/${id}`)}> Go Back</div>
       </div>
 
