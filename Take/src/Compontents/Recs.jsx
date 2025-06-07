9be8c7a202319d1b337cd6c807b7b38396f3f7c6
Sumit Roy <147255172+sum1t7@users.noreply.github.com>
2025-06-07 07:19:19 +0530

    url_update

--- Take/src/Compontents/Recs.jsx
+++ Take/src/Compontents/Recs.jsx
@@ -17,7 +17,7 @@ const Recs = () => {
 
     try {
       setIsLoading(true);
-      const response = await fetch(`https://gemma-endpoint.vercel.app/api/prompt`, {
+      const response = await fetch(`https://gemma-endpoint-fqc8.vercel.app/api/prompt`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
